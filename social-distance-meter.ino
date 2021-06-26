5b72fbdfc9bdc1c171a20318443b8b20f1b85ebb
Michael Hourican <mhourican01@qub.ac.uk>
2021-06-26 13:53:27 +0100

    Added functions

--- social-distance-meter.ino
+++ social-distance-meter.ino
@@ -1,35 +1,38 @@
 #include <LiquidCrystal.h>
 
 // LCD pins
-int rs = 7;
-int en = 8;
-int d4 = 9;
-int d5 = 10;
-int d6 = 11;
-int d7 = 12;
+const int rs = 7;
+const int en = 8;
+const int d4 = 9;
+const int d5 = 10;
+const int d6 = 11;
+const int d7 = 12;
 
 // Ultrasonic sensor pins
-int trigPin = 3;
-int echoPin = 2;
+const int trigPin = 3;
+const int echoPin = 2;
 
 // Output component pins
-int greenPin = 4;
-int redPin = 5;
-int buzzPin = 6;
+const int greenPin = 4;
+const int redPin = 5;
+const int buzzPin = 6;
 
 int travelT;
 float travelDist;
 float targetDist;
 float distInM;
-float safeDist = 2.;
+const float safeDist = 2.;
 
-int delayT1 = 10;
-int delayT2 = 25;
-int delayT3 = 250;
+const int delayT1 = 10;
+const int delayT2 = 25;
+const int delayT3 = 250;
+const int buzzerDelay1 = 100;
+const int buzzerDelay2 = 500;
 
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 void setup() {
+  
   Serial.begin(9600);
   lcd.begin(16,2);
   pinMode(trigPin, OUTPUT);
@@ -40,7 +43,21 @@ void setup() {
 }
 
 void loop() {
-  // Sends and receives ultrasonic sound waves
+  
+  travelT = sendWave();
+
+  distInM = calculateDistance();
+
+  displayDistance();
+
+  warnUser();
+  
+  updateReading();
+}
+
+// Sends and receives ultrasonic sound waves
+int sendWave() {
+  
   digitalWrite(trigPin, LOW);
   delayMicroseconds(delayT1);
   digitalWrite(trigPin, HIGH);
@@ -49,18 +66,30 @@ void loop() {
   travelT = pulseIn(echoPin, HIGH);
   delayMicroseconds(delayT2);
 
-  // Calculates distance (in inches) from time using dimensional analysis
-  travelDist = (travelT * 765. * 5280. * 12) / (3600. * 1000000);
+  return travelT;
+}
+
+// Calculates distance (in inches) from time using dimensional analysis and speed of sound
+float calculateDistance() {
+    
+  travelDist = (travelT * 767. * 5280. * 12) / (3600. * 1000000);
   targetDist = travelDist / 2;
   // Converts inches to meters
   distInM = (targetDist * 2.54) / 100;
+  return distInM;
+}
 
-  // Displays distance in m
+// Displays distance in m
+void displayDistance() {
+   
   lcd.setCursor(0,0);
   lcd.print("Distance: ");
   lcd.print(distInM);
   lcd.print("m");
+}
 
+// Warn user whether their distance from object is safe or unsafe
+void warnUser() {
   // If object is 2m or further,
   // displays 'safe' message, and green LED
   if (distInM >= safeDist) {
@@ -78,9 +107,14 @@ void loop() {
     digitalWrite(greenPin, LOW);
     digitalWrite(redPin, HIGH);
     digitalWrite(buzzPin, HIGH);
+    delay(buzzerDelay1);
+    digitalWrite(buzzPin, LOW);
+    delay(buzzerDelay2);
   }
-  
-  // Updates distance every quarter second
+}
+
+// Updates distance every quarter second, and resets display
+void updateReading() {
   delay(delayT3);
   lcd.clear(); 
 }
